482f9452d701849902ea1ec0662ed3f328310588
Mikhail Arkhipov <TeralGoe@users.noreply.github.com>
2019-12-07 22:15:41 +0300

    Solution for Day1::Part2

--- 2019/day_1/solution.rb
+++ 2019/day_1/solution.rb
@@ -4,9 +4,27 @@
 # ~ https://adventofcode.com/2019/day/1
 # *
 class DayOne
-  def part_one(f)
-    [*IO.foreach(f)].map { |n| n.to_i / 3 - 2 }.reduce(:+)
+  attr_accessor :i
+
+  def initialize
+      self.i = [*IO.foreach('input')]
+  end
+
+  def solution(r = false)
+    i.map { |n| r ? _r(n) : _m(n) }.reduce(:+)
+  end
+
+  def _r(n)
+      x = _m(n)
+      return x unless x > 0 && _r(x) > 0
+      x += _r(x)
+  end
+
+  def _m(n)
+    n.to_i / 3 - 2
   end
 end
 
-p "Part one: #{DayOne.new.part_one('input_1')}"
+result = DayOne.new
+p "Part one: #{result.solution}"
+p "Part two: #{result.solution(r = true)}"
